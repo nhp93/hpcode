6492ac05ef14fec10e7e2999f296c8ecce31f651
Phuc <nghongphuc93@gmail.com>
2024-08-25 17:41:16 +0700

    update

--- src/constants/blog.ts
+++ src/constants/blog.ts
@@ -58,30 +58,70 @@ export const dataBlogs: Blog[] = [
   },
   {
     id: 2,
-    author: "HUYỀN TRANG",
+    author: "Trung Anh Hoang",
     thumnail:
-      "https://s3-alpha-sig.figma.com/img/449e/d22e/31636dceffc9c2a85517b6cccf9eb77e?Expires=1722211200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=fFlqMRzgEOonMTqhIA4lGLoMyO~NoS-7J95zp083zriox1PioZC5B4w2AB8bI1WDUjr8xG01lvt49LHwb-xzpWOTb1LauB2-OhPTXHyhLfOt~aHljKiFQbSoEvRTDjOVtsUlw4sXvCoUCxqTGGfFZs7noWPWiJwyFaUZg-OC1XaCAhvQcvx4jWwdHufxy1~ofXu0LyhxT540fNxkQe3DSvI-D~NtLVFwPGGjF5ixZR7ntKKPZjEsmBZflBci6gLpsFV-Tgb1TSrfdd9BWd4YSj9N-8t0NELrcSFZm3othlpqmH3N2qXx6QZpVLvixU5Itik-bFJJ35VOH8EXAyjpog__",
-    title: "Câu chuyện đầy cảm hứng của Huyền Trang",
+      "https://www.kymviet.com.vn/Qu%C3%A0%20t%E1%BA%B7ng%20k%E1%BB%B7%20ni%E1%BB%87m%20s%E1%BB%B1%20ki%E1%BB%87n.jpg",
+    title: "Quà tặng kỷ yếu và những gợi ý quà tặng có ý nghĩa",
     description:
-      "Là tình nguyện viên của UNICEF, công việc của Huyền Trang là thúc đẩy quyền của trẻ em khuyết tật. Bài viết dưới đây sẽ chia sẻ cho bạn đọc về công việc cũng như cảm xúc của cô khi đồng hành cùng các hoạt động của UNICEF",
+      "Quà tặng kỷ yếu là những món quà đặc biệt được trao tặng nhân dịp kỷ niệm thành lập, những cột mốc quan trọng của một tổ chức xã hội hay của doanh nghiệp, hoặc để tri ân đối tác, khách hàng. Đây không chỉ là một món quà vật chất mà còn mang ý nghĩa tinh thần sâu sắc, thể hiện sự gắn kết, tri ân",
     dateTime: "10 ngày trước",
-    day: "06/06/2024",
+    day: "01/08/2024 06:25 PM",
     subContent: [
       {
         id: 1,
         imageUrl:
-          "https://s3-alpha-sig.figma.com/img/b0d8/d56f/c1806f8883cbd4c5998f024877c5c3dd?Expires=1724025600&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=chBwMqRwQ3QcpdnOWqGo4bNLveTN7k02KhlgBxXquDyqgGd2RnuQSM87y27KtbiV1lV0wq7-BjXdbdgVjTjF-YcYHFQTEe4dQEBM52gokeqSE4Iy4rfHeRKGQgEmK8IepgDpG1dYXZcc0b6lBWogMzMclZ57LhkNgxgQkCQ9FbxK~MGQZSiXveT-JTgBmAL~UhguyGCeYkO4NKdMeMd4JrtZYfyx0eWLfcm~6vzDGesFQvBdcACLEX4WXNjzhMKBHM-NMAchV7psP4353qnJyxdgj0s19ttftwtY~wwX~ZMwOWljsl4AEiCKROO~SpWREe5VIPrWXGzfbY3XZxZwMA__",
-        descImage:
-          "Đoàn công tác đang chia sẻ về các chính sách pháp luật và hoà nhập khuyết tật",
-        content: `Ngày 03-04/5/2024, Viện ACDC phối hợp cùng Sở Lao động – Thương binh và Xã hội, Trung tâm Trợ giúp pháp lý tỉnh và các tổ chức hỗ trợ người khuyết tật tỉnh Bình Định tổ chức tư vấn pháp luật và hoà nhập xã hội cho người khuyết tật huyện Tây Sơn. Sự kiện được diễn ra trong khuôn khổ dự án Hỗ trợ cải thiện chất lượng sống của người khuyết tật tại các tỉnh bị phun rải nặng chất da cam tại tỉnh Bình Định.\nĐoàn tư vấn pháp luật và hỗ trợ hoà nhập có sự tham gia của bà Vũ Thị Tuyết Mai – Giám đốc dự án; bà Lê Hải Yến, Trưởng phòng Luật Viện ACDC; bà Nguyễn Thị Thúy Hằng, đại diện Sở Lao động – Thương binh và Xã hội tỉnh; ông Lê Thành Sơn – Phó Giám đốc Trung tâm Trợ giúp pháp lý nhà nước tỉnh; ông Bùi Trung Dũng – Phó Chủ tịch Hội Bảo trợ người khuyết tật và Bảo vệ quyền trẻ em tỉnh; bà Nguyễn Thị Kim Vân – Phó Giám đốc Trung tâm giáo dục nghề nghiệp tỉnh; bà Trần Thị Thúy Nga – Phó Giám đốc Phòng Giao dịch Ngân hàng Chính sách xã hội huyện Tây Sơn.\nMục tiêu của hoạt động nhằm nâng cao hiểu biết cho người khuyết tật hoặc gia đình người khuyết tật về các chính sách, pháp luật hiện hành liên quan đến người khuyết tật; giúp tư vấn, giải đáp trực tiếp các câu hỏi, vụ việc liên quan đến pháp luật như cho người khuyết tật hoặc gia đình của người khuyết tật có nhu cầu.\nChia sẻ tại buổi tư vấn, ông Nguyễn Hữu Vinh, Trưởng phòng pháp luật về lao động xã hội, Trung tâm Trợ giúp pháp lý tỉnh Bình Định: Người dân đến tham gia hoạt động rất đông, và có nhiều thắc mắc không hiểu, cần được giải đáp. Người khuyết tật và người thân do tôi tư vấn đa phần là vấn đề thừa kế, về thủ tục làm giấy chứng nhận quyền sử dụng đất, tài sản trên đất… Hình thức tư vấn trực tiếp như thế này vô cùng hiệu quả và thiết thực với người khuyết tật.`,
+          "https://www.kymviet.com.vn/Qu%C3%A0%20t%E1%BA%B7ng%20k%E1%BB%B7%20ni%E1%BB%87m%20s%E1%BB%B1%20ki%E1%BB%87n.jpg",
+        descImage: "",
+        content: `Quà tặng kỷ yếu là những món quà đặc biệt được trao tặng nhân dịp kỷ niệm thành lập, những cột mốc quan trọng của một tổ chức xã hội hay của doanh nghiệp, hoặc để tri ân đối tác, khách hàng.
+Quà tặng kỷ yếu không chỉ là một món quà vật chất mà còn mang ý nghĩa tinh thần sâu sắc, thể hiện sự gắn kết, tri ân và lưu trữ những mốc kỷ niệm đẹp
+
+Ý nghĩa quan trọng của quà tặng kỷ yếu :
+
+✔️ Thúc đẩy tinh thần đoàn kết: Quà tặng giúp tăng cường sự gắn kết giữa các thành viên trong công ty, tạo ra không khí làm việc tích cực.
+
+✔️ Biểu hiện sự trân trọng: Đây là cách để công ty bày tỏ lòng biết ơn đối với những đóng góp của nhân viên và đối tác.
+
+✔️ Xây dựng hình ảnh thương hiệu: Quà tặng kỷ yếu độc đáo, ấn tượng sẽ góp phần nâng cao hình ảnh nhận diện của tổ chức hay doanh nghiệp.
+
+✔️ Tạo dấu ấn khó quên: Những món quà ý nghĩa sẽ được lưu giữ và trở thành kỷ niệm đẹp trong lòng người nhận.`,
       },
       {
         id: 2,
         imageUrl:
-          "https://s3-alpha-sig.figma.com/img/433c/42af/8f00633cf3ad3948ac1fd4b1f9b48b2e?Expires=1724025600&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=HqBy209rek5vAr7PipdOUY5dxsElHDF7Kg~aMDOOS4SahN-~jPkfKg7g9a6ygsAcoUMkjdjt9P-pqc5csMXg5ZZG5l7820KpXRcin9T~dp-FWVnrVwluPHW1zt-eVhk7DruLB2Lw1XayV80NB8HkldZ~oO7G30rKx7OJaEe1qeBZs9OtNmjxLQ8KX1Bycz3IwyFPfPDAgM4TZ~EvBRa32KhJ0URMx9IselOYdM9AIgW~Nbb6rktwUftFw8hhSV~LIEsC2rogqPTCsljaphMXd6WLYwTsyo3SSfst-aML8yz~PHeNKjH0euT6RoVfBYTnAq5extgKypQsEmgAkBdvJQ__",
+          "https://www.kymviet.com.vn/Du%20l%E1%BB%8Bch%20c%E1%BB%99ng%20%C4%91%E1%BB%93ng%20tr%E1%BA%A3i%20nghi%E1%BB%87m%20v%C4%83n%20h%C3%B3a%20truy%E1%BB%81n%20th%E1%BB%91ng%20b%E1%BA%A3n%20%C4%91%E1%BB%8Ba.jpg",
         descImage:
           "Đoàn công tác đang chia sẻ về các chính sách pháp luật và hoà nhập khuyết tật",
-        content: `Chia sẻ sau khi được tư vấn, chị N.T.N.L, người nhà của người khuyết tật tại xã Bình Nghi cho biết: Tôi hôm nay đến với mong muốn giải đáp câu hỏi về tăng trợ cấp cho người khuyết tật. Sau khi được tư vấn, tôi hiểu rõ được mức trợ cấp hiện tại là mức cao nhất của tỉnh. Tôi rất hài lòng với cách tư vấn tận tình và cặn kẽ của đoàn.\n\nÔng V.T, người khuyết tật xã Bình Hoà nói: Từ lâu, tôi đã băn khoăn về quyền thừa kế của mình nhưng không biết hỏi ai. Giờ được đoàn tư vấn quy trình thực hiện, tôi sẽ làm theo hướng dẫn và biết tìm đến đâu để được hỗ trợ.\nChị T.T.C, người khuyết tật vui mừng chia sẻ: Đây là lần đầu tiên tôi được tham gia một sự kiện như thế này. Buổi tư vấn vô cùng bổ ích vì được giải đáp từng vấn đề riêng của từng người. Tôi mong có nhiều hơn nữa hoạt động như thế này, vì người khuyết tật chúng tôi tiếp cận thông tin rất hạn chế. Tôi vô cùng biết ơn đoàn tư vấn đã đến tận nơi, hỗ trợ tận tình cho chúng tôi.\nHoạt động nằm trong khuôn khổ dự án “Hỗ trợ cải thiện chất lượng sống của người khuyết tật tại các tỉnh bị phun rải nặng chất da cam” (gọi tắt là Dự án Hoà nhập II) tại tỉnh Bình Định và Kon Tum giai đoạn 2023 – 2026, do Cơ quan Phát triển Quốc tế Hoa Kỳ (USAID) tài trợ, Trung tâm hành động quốc gia khắc phục hậu quả chất độc hóa học và môi trường (NACCET) thuộc Bộ Quốc phòng làm Chủ dự án. Viện ACDC là một trong các đối tác triển khai các hoạt động dưới sự quản lý của Humanity & Inclusion (HI).`,
+        content: `Dưới đây là một số gợi ý quà tặng kỷ yếu doanh nghiệp ý nghĩa theo các nhóm:
+
+1.  Quà tặng kỷ yếu mang tính biểu tượng:
+
+✔️ Sản phẩm từ gỗ: Đồng hồ gỗ, móc treo chìa khóa…được in lazer hoặc khắc 3D hình ảnh, họa tiết biểu trưng mang vẻ đẹp nghệ thuật và sang trọng.
+
+✔️ Kỷ niệm chương: Với thiết kế sang trọng, khắc tên và lời chúc của công ty.
+
+✔️ Bình đựng nước, cốc sứ: In logo và thông điệp ý nghĩa.
+
+2.  Quà tặng kỷ yếu thể hiện giá trị văn hóa doanh nghiệp:
+
+✔️ Đồ thủ công mỹ nghệ: Các sản phẩm mang đậm nét văn hóa truyền thống hoặc hiện đại chất liệu vải, nhồi bông, gỗ tùy thuộc vào đặc trưng của doanh nghiệp.
+
+✔️ Bộ sản phẩm chăm sóc sức khỏe: Gọn gàng, tiện lợi và thể hiện sự quan tâm của công ty đến sức khỏe của nhân viên.
+
+✔️ Túi sách canvas: Được làm từ vải canvas bền chắc, túi vải canvas được in logo không chỉ là tư trang chứa vật dụng cá nhân mà ý nghĩa quảng bá thương hiệu hay tạo sự nhận diện thương hiệu cho các công ty hoặc sự kiện quan trọng nào đó.
+3.  Quà tặng trải nghiệm:
+
+✔️ Voucher nghỉ dưỡng: Tặng nhân viên và đối tác những chuyến đi nghỉ dưỡng để thư giãn và tái tạo năng lượng.
+
+✔️ Khóa học nâng cao kỹ năng: Đầu tư vào việc phát triển năng lực của nhân viên.
+
+✔️ Sự kiện trải nghiệm văn hóa cộng đồng: Tổ chức các hoạt động giao lưu văn hóa, văn nghệ, gắn kết để tăng cường tinh thần đồng đội`,
+      },
+      {
+        id: 3,
+        imageUrl:
+          "https://i.pinimg.com/736x/42/9c/f2/429cf22bcfc7ec6e658fd617bda18b88.jpg",
+        descImage: "123",
+        content: "456",
       },
     ],
   },
@@ -90,7 +130,7 @@ export const dataBlogs: Blog[] = [
     author: "ĐẶNG THÙY LINH",
     thumnail:
       "https://s3-alpha-sig.figma.com/img/25ba/9bf1/f50e02f03e31ac2c49e8ba3269f09a1d?Expires=1722211200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=HOxxIrdolrC0jsnVlnJ5hlDlGbw4IzTKC7Yp3oHvsLdkL8g8IfX9H4c1zfR9KzGwTuoCBM7PCmEbwvZmRlTKDIcaACUHqb1r8TSG3Dlx66KcygJ2PzbVNUPI6FC493H3DuNeBWM9b1Vf2ZPZZYBCpzSG331eKMhYhidzGzxjL7yZlhxZQXfjD7Ap9EQyZOxzzUwgZBOXpTzxY~efOA8DpQO2R7eYz7eCUiZgkpc4wenXPXytqU5xWmbcqodWyu3rSB7XnAg76pdndRdzoDbvJXlo4hmqfQjaOsBd-zyCyMS4t9q6OfVCIlqeDJk029UQG0F5Dwup-MT-r7AumxsEtg__",
-    title: "Câu chuyện của Mai làm thúc đẩy nghề công tác xã hội tại Việt Nam",
+    title: "SẢN PHẨM THỦ CÔNG HANDMADE VÀ HANDICRAFT KHÁC NHAU THẾ NÀO?",
     description:
       "Mình là một người phụ nữ mang trên mình khuyết tật về thể chất. Mình sinh ra và lớn lên tại Hà Nội, thủ đô của Việt Nam. Chuyện kể rằng, mình được sinh ra vào thời kỳ sau chiến tranh. ",
     dateTime: "12 phút trước",
--- src/pages/Introduction/index.tsx
+++ src/pages/Introduction/index.tsx
@@ -3,60 +3,78 @@ import IntroductionImages from "@/components/img/Introduction";
 import React from "react";
 
 const Introduction = () => {
-	const introductions = [
-		{
-			img: IntroductionImages.whoAreUs,
-			title: "Chúng tôi là ai?",
-			subtitle: "Lorem ipsum dolor sit amet consectetur. ",
-			detail: "Lorem ipsum dolor sit amet consectetur. Turpis tristique amet aliquam a. Augue venenatis praesent tellus at. Arcu imperdiet interdum consectetur dui egestas pharetra leo arcu. Cum sed duis velit vitae. Vulputate scelerisque sit convallis arcu risus leo. Felis convallis non aliquam ipsum amet sed eleifend purus eu. Justo facilisi sollicitudin porttitor lacus ut nunc sed tristique id.",
-			flex: "flex md:flex-row",
-		},
-		{
-			img: IntroductionImages.goal,
-			title: "Mục tiêu chiến lược",
-			subtitle: "Lorem ipsum dolor sit amet consectetur. ",
-			detail: "Lorem ipsum dolor sit amet consectetur. Turpis tristique amet aliquam a. Augue venenatis praesent tellus at. Arcu imperdiet interdum consectetur dui egestas pharetra leo arcu. Cum sed duis velit vitae. Vulputate scelerisque sit convallis arcu risus leo. Felis convallis non aliquam ipsum amet sed eleifend purus eu. Justo facilisi sollicitudin porttitor lacus ut nunc sed tristique id.",
-			flex: "flex md:flex-row-reverse",
-		},
-		{
-			img: IntroductionImages.action,
-			title: "Hoạt động ưu tiên",
-			subtitle: "Lorem ipsum dolor sit amet consectetur. ",
-			detail: "Lorem ipsum dolor sit amet consectetur. Turpis tristique amet aliquam a. Augue venenatis praesent tellus at. Arcu imperdiet interdum consectetur dui egestas pharetra leo arcu. Cum sed duis velit vitae. Vulputate scelerisque sit convallis arcu risus leo. Felis convallis non aliquam ipsum amet sed eleifend purus eu. Justo facilisi sollicitudin porttitor lacus ut nunc sed tristique id.",
-			flex: "flex md:flex-row",
-		},
-	];
-	return (
-		<div className="w-screen mt-16">
-			{/* header */}
-			<Banner
-				title="Giới thiệu về câu lạc bộ của chúng tôi"
-				image={IntroductionImages.banner}
-			/>
-			{/* content */}
-			<div className="md:pt-24 md:pb-[128px] md:px-40 mobile:px-5 mobile:pt-12 mobile:pb-16">
-				{introductions.map((introduction, index) => (
-					<div
-						className={`mobile:flex mobile:flex-col-reverse mobile:gap-6 md:${introduction.flex} w-full md:gap-16 mb-16`}
-						key={index}
-					>
-						<div className="md:py-5 flex-1 text-justify">
-							<h3 className="text-primary text-left uppercase mobile:text-[16px] md:text-lg font-semibold font-inter mb-3">{introduction.title}</h3>
-							<h1 className="md:text-[28px] text-left mobile:text-[24px] font-semibold font-inter mb-3">{introduction.subtitle}</h1>
-							<p className="mobile:text-[16px] md:text-lg font-inter">{introduction.detail}</p>
-						</div>
-						<div className="flex-1">
-							<img
-								src={introduction.img}
-								alt=""
-								className="w-full h-full object-cover rounded-2xl"
-							/>
-						</div>
-					</div>
-				))}
-			</div>
-		</div>
-	);
+  const introductions = [
+    {
+      img: IntroductionImages.whoAreUs,
+      title: "Sứ mệnh và tầm nhìn",
+      subtitle: "Tổng quan về mục tiêu trang web ",
+      detail: [
+        "Sứ mệnh của chúng tôi là trở thành cầu nối giữa những doanh nhân khuyết tật và người tiêu dùng, từ đó tạo cơ hội kinh doanh công bằng và thúc đẩy sự hoà nhập xã hội. Chúng tôi tin rằng với những công cụ và sự hỗ trợ phù hợp, bất kỳ ai cũng có thể trở thành một doanh nhân thành công. Tầm nhìn của chúng tôi là xây dựng một cộng đồng kinh doanh nơi những người khuyết tật không chỉ phát triển mà còn trở thành những người tiên phong trong việc tạo ra thay đổi tích cực trong xã hội.",
+      ],
+      flex: "flex md:flex-row",
+    },
+    {
+      img: IntroductionImages.goal,
+      title: "Tính Năng",
+      subtitle: "Các dịch vụ chính",
+      detail: [
+        "Chúng tôi cung cấp một loạt các dịch vụ giúp người khuyết tật xây dựng và quản lý cửa hàng trực tuyến của mình một cách hiệu quả. Những dịch vụ bao gồm: ",
+        "- Tạo gian hàng miễn phí: Mỗi người khuyết tật có thể dễ dàng thiết lập một cửa hàng trực tuyến với giao diện thân thiện và dễ sử dụng.",
+        "- Hỗ trợ tiếp thị và quảng bá sản phẩm: Chúng tôi cung cấp công cụ quảng cáo và tiếp thị mạnh mẽ để giúp sản phẩm của người khuyết tật tiếp cận đến đúng đối tượng khách hàng.",
+        "- Dịch vụ tư vấn kinh doanh: Đội ngũ chuyên gia của chúng tôi sẵn sàng hỗ trợ, từ việc phát triển sản phẩm đến chiến lược bán hàng và chăm sóc khách hàng.",
+      ],
+      flex: "flex md:flex-row-reverse",
+    },
+    {
+      img: IntroductionImages.action,
+      title: "Chủng tôi",
+      subtitle: "Mong muốn đem lại lợi ích những mảnh đời thiếu may mắn",
+      detail: [
+        "Chúng tôi sẽ giúp quảng bá, mang những sản phẩm của những con người kém may mắn trở nên phổ biến hơn với cộng đồng. ",
+        "Người khuyết tật không chỉ có cơ hội giới thiệu về những mặt hàng của bản thân, giúp cộng đồng có một cái nhìn tích cực hơn, thấu hiểu và đồng cảm sâu sắc với những khao khát được làm việc, được cống hiến và làm chủ cuộc sống của người khuyết tật",
+      ],
+      flex: "flex md:flex-row",
+    },
+  ];
+  return (
+    <div className="w-screen mt-16">
+      {/* header */}
+      <Banner
+        title="Giới thiệu về câu lạc bộ của chúng tôi"
+        image={IntroductionImages.banner}
+      />
+      {/* content */}
+      <div className="md:pt-24 md:pb-[128px] md:px-40 mobile:px-5 mobile:pt-12 mobile:pb-16">
+        {introductions.map((introduction, index) => (
+          <div
+            className={`mobile:flex mobile:flex-col-reverse mobile:gap-6 md:${introduction.flex} w-full md:gap-16 mb-16`}
+            key={index}
+          >
+            <div className="md:py-5 flex-1 text-justify">
+              <h3 className="text-primary text-left uppercase mobile:text-[16px] md:text-lg font-semibold font-inter mb-3">
+                {introduction.title}
+              </h3>
+              <h1 className="md:text-[36px] text-left mobile:text-[24px] font-semibold font-inter mb-3">
+                {introduction.subtitle}
+              </h1>
+              <div className="mobile:text-[16px] md:text-xl font-inter">
+                {introduction.detail.map((paragraph, idx) => (
+                  <p key={idx}>{paragraph}</p>
+                ))}
+              </div>
+            </div>
+            <div className="flex-1 h-[420px]">
+              <img
+                src={introduction.img}
+                alt=""
+                className="w-full h-full object-cover bg-top rounded-2xl"
+              />
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Introduction;
