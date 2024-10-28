53fadd57e1e48f3a90985ffbdbcefbff367ea652
Phuc <nghongphuc93@gmail.com>
2024-10-29 00:25:32 +0700

    final 28/10

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import RouterConfig from "@/routes/RouterConfig";
 import { AppProvider } from "./providers/app";
+import ScrollToTop from "./components/ScrollToTop";
 
 function App() {
   return (
     <div className="App">
       <AppProvider>
         <RouterConfig />
+        <ScrollToTop />
       </AppProvider>
     </div>
   );
